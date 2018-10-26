5f81d23b3af613d3a01f0cf7c1812517af8e46b0
Kirill Staroselskiy <Staroselskiy.k@gmail.com>
2018-10-26 12:58:00 +0300

    Создание UI класса для ввода смс кода

---
+++ AmicortApp/AmicortApp/UIClasses/Login/SelectSmsCodeVC.swift
@@ -0,0 +1,40 @@
+//
+//  SelectSmsCodeVC.swift
+//  AmicortApp
+//
+//  Created by Kirill on 26/10/2018.
+//  Copyright © 2018 Kirill. All rights reserved.
+//
+
+import UIKit
+
+class SelectSmsCodeVC: UIViewController {
+
+    @IBOutlet weak var informText: UILabel!
+    
+    @IBOutlet weak var selectCode: UITextField!
+    
+    @IBOutlet weak var enter: UIButton!
+    
+    @IBOutlet weak var repeatSmsCode: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
